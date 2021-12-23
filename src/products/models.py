d785867a7beb0a69dee610c9a663d1024f66a7d5
akhundMurad <akhundov1murad@gmail.com>
2021-12-23 20:34:14 +0400

    created Order model products = models.ManyToManyField total_cost_price = models.PositiveIntegerField total_price = models.PositiveIntegerField created_at = models.DateTimeField products_quantity = models.PositiveIntegerField

--- src/products/models.py
+++ src/products/models.py
@@ -10,3 +10,34 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        CANCELED = 'canceled', 'отменен'
+        ON_PROCESSING = 'on_processing', 'на обработке'
+
+    status = models.CharField(
+        choices=Status.choices,
+        max_length=13,
+        verbose_name='статус'
+    )
+    products = models.ManyToManyField(
+        'products.Product',
+        verbose_name='продукты'
+    )
+    total_cost_price = models.PositiveIntegerField(
+        verbose_name='итоговая себестоимость'
+    )
+    total_price = models.PositiveIntegerField(verbose_name='итоговая цена')
+    created_at = models.DateTimeField(
+        auto_created=True,
+        verbose_name='дата создания'
+    )
+    products_quantity = models.PositiveIntegerField(
+        verbose_name='количество продуктов'
+    )
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
