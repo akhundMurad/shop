14bb444880bf646add538b50f5b32f0992681e75
akhundMurad <akhundov1murad@gmail.com>
2021-12-23 20:25:10 +0400

    created Product model name = models.CharField cost_price = models.PositiveIntegerField price = models.PositiveIntegerField quantity = models.IntegerField

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=512, verbose_name='название')
+    cost_price = models.PositiveIntegerField(verbose_name='себестоимость')
+    price = models.PositiveIntegerField(verbose_name='цена')
+    quantity = models.IntegerField(verbose_name='количество')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
