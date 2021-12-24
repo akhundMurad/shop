0a9cd0ae94a520f8f58ea048a9a907bd17e2087f
akhundMurad <akhundov1murad@gmail.com>
2021-12-25 00:40:47 +0400

    exception handler

--- src/products/views/orderedproduct.py
+++ src/products/views/orderedproduct.py
@@ -4,9 +4,10 @@ from rest_framework.views import APIView
 
 from products.models import Product, OrderedProduct
 from products.services.orderedproduct import bulk_create
+from utils.exceptions import ErrorHandlerMixin
 
 
-class OrderedProductBulkCreateAPIView(APIView):
+class OrderedProductBulkCreateAPIView(ErrorHandlerMixin, APIView):
     class InputSerializer(serializers.Serializer):
         product = serializers.PrimaryKeyRelatedField(
             queryset=Product.objects.all()
@@ -17,6 +18,8 @@ class OrderedProductBulkCreateAPIView(APIView):
         class Meta:
             model = OrderedProduct
             fields = ('id', 'product', 'order', 'product_quantity')
+
+    model = OrderedProduct
 
     def post(self, request) -> Response:
         ordered_products = bulk_create(
