610136fd042134dbeee1c8617feffe1103483d39
akhundMurad <akhundov1murad@gmail.com>
2021-12-26 13:49:54 +0400

    created Report model product = models.ForeignKey proceeds = models.PositiveIntegerField earnings = models.IntegerField number_of_sold = models.PositiveIntegerField models.PositiveIntegerField created_at = models.DateField

--- src/reporting/models.py
+++ src/reporting/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    class Meta:
+        verbose_name = 'отчет'
+        verbose_name_plural = 'отчеты'
+
+    product = models.ForeignKey(
+        'products.Product',
+        on_delete=models.CASCADE,
+        verbose_name='продукт'
+    )
+
+    proceeds = models.PositiveIntegerField(
+        default=0,
+        verbose_name='выручка'
+    )
+    earnings = models.IntegerField(
+        default=0,
+        verbose_name='прибыль'
+    )
+    number_of_sold = models.PositiveIntegerField(
+        default=0,
+        verbose_name='количество проданных'
+    )
+    number_of_canceled = models.PositiveIntegerField(
+        default=0,
+        verbose_name='количество возвратов'
+    )
+
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name='дата создания'
+    )
